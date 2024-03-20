5b717ca05bc9bef3f4ad6b8029e0f1deeb43f7b2
Cleveland Martin IV <tsarycleveland@gmail.com>
2024-03-19 23:31:15 -0500

    Completed

--- SoundAssignments/Synthesizer2/sketch.js
+++ SoundAssignments/Synthesizer2/sketch.js
@@ -1,12 +1,8 @@
-//create a synth and connect it to the main output (your speakers)
+let synth = new Tone.Synth(Tone.Synth).toDestination();
+let lfo = new Tone.LFO(25, 1500, 100).connect(synth.frequency).start();
+let mouse;
+let pop;
 
-const synth = new Tone.PolySynth(Tone.Synth);
-const bend = new Tone.Reverb(); // adding pitch shifting
-let decay;
-bend.decay = 0.01;
-synth.connect(bend);
-bend.toDestination();
-//connecting synth to effect to destination
 
 let notes = {
   'a': 'C4',
@@ -19,33 +15,38 @@ let notes = {
   'k': 'C5'
 }
 
+function preload(){
+  mouse = loadImage('balloon.png');
+  pop = loadImage('pop.png');
+}
+
+
 function setup() {
   createCanvas(400, 400);
-  
-  decaySlider = createSlider (5., 19., 9., .01);
-  decaySlider.position (120,200);
-  decaySlider.mouseMoved(() => {
-    bend.decay = decaySlider.value();
-  })
- // mouseMoved is so the bend works while mouse is moving
 }
 
 function draw() {
-  background(100);
-  text("Play A through K and change reverb delay with slider.", 45, 150);
-  print(bend.decay);
+  if (mouseIsPressed === true){
+    background(255);
+    background(pop);
+    
+  } 
+  else if (mouseIsPressed === false){
+    background(255);
+    background (mouse);
+    textSize(38);
+    textStyle(BOLD);
+    text ('Click to Pop Balloon!', 7, height-160);
+    textSize(15);
+    textStyle(ITALIC);
+    text ('(You May NOT Want to Hold The Mouse Down...)', 20, height-110);
+  }
 }
 
-
-
-function keyPressed() {
-  let playNotes = notes[key]; // assign notes variable to keys when played
-  synth.triggerAttack(playNotes);
-// separated attack and release so keyboard press controls this
+function mousePressed() { 
+    synth.triggerAttack();
 }
 
-function keyReleased() {
-  let playNotes = notes[key]; // assign notes variable to keys when played
-  synth.triggerRelease(playNotes, '+0.03'); 
-  // added time to release to reduce popping sound
-}
+function mouseReleased() { 
+  synth.triggerRelease();
+} 
